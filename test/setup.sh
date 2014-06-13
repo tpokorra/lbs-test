84c28f814b6185d51e30d265da2511e3f20420d6
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-06-13 13:40:42 +0200

    create sample tarball

--- test/setup.sh
+++ test/setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 function buildTarBall {
-    echo "LBSERROR: no tarball was created"
-    exit 1
+    #echo "LBSERROR: no tarball was created"
+    #exit 1
+
+    # create the tarball
+    mkdir test-1.0.0
+    cd test-1.0.0
+    echo "hello world" > test.txt
+    cd ..
+    tar cjf test.tar.bz2 test-1.0.0
+    mv test.tar.bz2 ~/sources
 }
 
 mkdir ~/sources
